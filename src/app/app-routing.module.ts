79ef381992eb6aa187d9258ae6b2edc4e132e2e5
vitorliras <vitorlirasti@hotmail.com>
2024-05-26 14:22:52 -0300

    studylog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,11 @@
-import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastroComponent } from './cadastro/cadastro.component';
+import { ConteudoComponent } from './pages/conteudo/conteudo.component';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'cadastro',
-  },
-  {
-    path: 'cadastro',
-    component: CadastroComponent
-  },
-  {
-    path: 'sucesso',
-    component: SucessoCadastroComponent
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: ConteudoComponent },
 ];
 
 @NgModule({
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,12 +8,29 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  menuOpen = false;
+  options = [
+    { title: 'Início', endpoint: '/' },
+    { title: 'Conhecimentos Especifícos', endpoint: '/conhecimentos-especificos' },
+    { title: 'Noções Criminalista', endpoint: '/noces-criminalista' },
+    { title: 'Noções de Medicina Legal', endpoint: '/noces-medicina-legal' },
+    { title: 'Noções de Direito Penal', endpoint: '/noces-direito-penal' },
+    { title: 'Noções De Direito Processual Penal', endpoint: '/noces-direito-processual-penal' },
+    { title: 'Língua Portuguesa', endpoint: '/lingua-portuguesa' },
+    { title: 'Raciocínio Lógico', endpoint: '/raciocinio-logico' },
+    { title: 'Noções de Documentos Técnicos', endpoint: '/noces-documentos-tecnicos' }
+  ];
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-  telaCadastro(){
-    this.router.navigate(['cadastro'])
+
+  tela(endpoint: string){
+    this.router.navigate([endpoint])
   }
 
 }
